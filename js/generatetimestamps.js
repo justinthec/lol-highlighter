bf8b6c687d80d6ef94f6a163614e4e5209954440
Antony Tsim <antonytsim@gmail.com>
2015-08-25 19:03:52 -0400

    Added merge timestamps function to combine overlapping timestamps.

--- js/generatetimestamps.js
+++ js/generatetimestamps.js
@@ -3,42 +3,46 @@ function getTimestamps(theUrl) {
     xmlHttp.open( "GET", theUrl, false );
     xmlHttp.send( null );
 
-    var startEndPair = new Array();
     var seperateKillLength = new Array();
-    var combinedKillLength = new Array();
-    var combinedStart;
-    var combinedEnd;
     var killCounter = 1;
     var allKills = "";
     var end;
     var gameData = JSON.parse(xmlHttp.responseText);
-    console.log(gameData);
 
     for (var i=0;i<gameData.frames.length;i++){
         for (var j=0;j<gameData.frames[i].events.length;j++) {
             if (gameData.frames[i].events[j].type=="CHAMPION_KILL") {
-                startEndPair = [];
-                start = Math.round((gameData.frames[i].events[j].timestamp - 10000)/1000);
-                end = Math.round((gameData.frames[i].events[j].timestamp+5000)/1000);
-                startEndPair.push(start, end);
+                var startEndPair = {};
+                startEndPair.start = Math.round((gameData.frames[i].events[j].timestamp - 10000)/1000);
+                startEndPair.end = Math.round((gameData.frames[i].events[j].timestamp+5000)/1000);
                 seperateKillLength.push(startEndPair);
             }
         }
     }
     for (var i=0;i<seperateKillLength.length;i++) {
-
-
-         for (var j=0;j<2;j++) {
-            startEndPair = [];
-
-            allKills += seperateKillLength[i][j] + "&nbsp&nbsp";
-        }
-        allKills += "Kills: " + killCounter + "<br>"; 
+                allKills += seperateKillLength[i].start + "&nbsp&nbsp" +  seperateKillLength[i].end + "&nbsp&nbsp";
+                allKills += "Kills: " + killCounter + "<br>"; 
     }
 
-
-
+    console.log(mergeTimestamps(seperateKillLength));
     document.getElementById("JSON").innerHTML = allKills;
 }
 
+function mergeTimestamps(timestamps) {
+    for (var i=0;i<timestamps.length-1;) {
+        if (timestamps[i].end >= timestamps[i+1].end) {
+            timestamps.splice(i+1,1);
+        }
 
+        else if (timestamps[i].end >= timestamps[i+1].start) {
+            var newStart = timestamps[i].start;
+            timestamps.splice(i,1);
+            timestamps[i].start = newStart;
+        }
+
+        else {
+            i++;
+        }
+    }
+    return timestamps;
+}
